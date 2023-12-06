15d3a582970b889d82082f0a5e1d749a9bc34c2d
erick <erick@toledo>
2023-12-06 16:43:13 -0500

    ciclo for...

--- ManejoCicloFor/src/paquete01/Ejemplo07.java
+++ ManejoCicloFor/src/paquete01/Ejemplo07.java
@@ -19,21 +19,21 @@ public class Ejemplo07 {
         String mensajeFinal = "";
         for (int i = 1; i <= 10; i++) { // for 01
             // System.out.printf("Tabla de multiplicar del número %d\n", i);
-            mensajeFinal = String.format("%sTabla de multiplicar del número %d\n", 
+            mensajeFinal = String.format("%sTabla de multiplicar del número %d\n",
                     mensajeFinal, i);
             for (int contador = 1; contador <= 12; contador++) {
                 operacion = i * contador;
-                //System.out.printf("%d x %d = %d\n", i, contador, operacion);
-                mensajeFinal = String.format("%s%d x %d = %d\n"
-                , mensajeFinal
-                , i
-                , contador
-                , operacion);
+                // podemos hacer un ciclo dentro de otro ciclo
+                // podemos cambiar o reasisgnar el valor de las variables cadena como lo estamos
+                // haciendo con mensajer final
+                mensajeFinal = String.format("%s%d x %d = %d\n", mensajeFinal, i, contador, operacion);
             }
             mensajeFinal = String.format("%s\n", mensajeFinal);
-        //System.out.println("\n");
-        }  //termina for 01
-        
+            // cada vez a mensaje final le reasignamos o sumamos un valor para que este
+            // puedea ser usado con mayor facicilad
+
+        }
+
         System.out.printf("%s\n", mensajeFinal);
     }
 }
--- ManejoCicloFor/src/paquete03/Ejemplo03.java
+++ ManejoCicloFor/src/paquete03/Ejemplo03.java
@@ -5,16 +5,57 @@
  */
 package paquete03;
 
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.StringJoiner;
+
+import javax.print.DocFlavor.STRING;
+
 /**
  *
  * @author reroes
  */
 public class Ejemplo03 {
-    
+
     public static void main(String[] args) {
-        
-        for (int i = 0; i < 10; i++) {
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+
+        String nombre;
+        String apellido;
+        String posicion;
+        int edad;
+        double estatura;
+        int jugador = 0;
+        String mensajeF = "";
+
+        System.out.println("Ingrese la cantidad de jugadores");
+        jugador = entrada.nextInt();
+
+        for (int i = 0; i < jugador; i++) {
+              entrada.nextLine();
+            System.out.println("Ingrese el nombre del Jugador: ");
+            nombre = entrada.nextLine();
             
+            System.out.println("Ingrese la posición en el campo: ");
+            posicion = entrada.nextLine();
+            System.out.println("Ingrese la edad del Jugador: ");
+            edad = entrada.nextInt();
+            System.out.println("Ingrese la estatura del Jugador");
+            estatura = entrada.nextDouble();
+
+            mensajeF = String.format("%s%s-%s-,edad %d, estatura %.2f\n"
+                    
+                    ,mensajeF,
+                    nombre,
+                
+                    posicion,
+                    edad,
+                    estatura);
+           
         }
+
+        System.out.printf("lista de jugadores\n%s",mensajeF);
+
     }
 }
